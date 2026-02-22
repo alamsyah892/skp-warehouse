13d7570d36561d8bdc427344b1b82caa6a8312bb
alamsyah892 <outinside20@outlook.com>
2026-02-23 00:06:25 +0700

    remove error theme

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ export default defineConfig({
             input: [
                 "resources/css/app.css",
                 "resources/js/app.js",
-                "vendor/andreia/filament-nord-theme/resources/css/theme.css",
                 "resources/css/filament/admin/theme.css",
             ],
             refresh: true,
